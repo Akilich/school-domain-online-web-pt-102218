dd945b05cd117737d8c7d59486a291e2e6e8937d
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-18 18:29:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,33 @@
 class School
-  roster = { }
-  def initialize(new)
-    @roster = roster
+  attr_reader :roster
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def name
+    @name
+  end 
+  
+  
+  def add_student(name, grade)
+    if roster.keys.include?(grade)
+      @roster[grade] << name
+    else   
+      @roster[grade] = [name]
+    end 
   end
-end
+    
+  def grade(grade)
+    @roster[grade] 
+  end 
+    
+  def sort
+    hash = {}
+    roster.sort.each do |grade, names_arr|
+      hash[grade] = names_arr.sort
+    end
+    hash.sort.to_h
+  end 
